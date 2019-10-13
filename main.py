7f88df89e25e7e065a4ef999bcd5f6db83e50b6e
jameswilliams1 <zcapjwi@ucl.ac.uk>
2019-10-13 13:39:20 +0100

    add sample output

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
 """Program entry point."""
 import pandas as pd
+
+
+def compare(df_1, df_2):
+    return pd.merge(pd.merge(df_1, df_2))
---
+++ test.py
@@ -0,0 +1,5 @@
+import pandas as pd
+from main import compare
+
+def test_compare():
+    data1 =
