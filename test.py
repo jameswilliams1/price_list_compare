4239fabb1e14b7b58b0a928b26c39d432738f904
jameswilliams1 <zcapjwi@ucl.ac.uk>
2019-10-13 14:28:12 +0100

    refactor

--- test.py
+++ test.py
@@ -4,8 +4,8 @@ from pandas.testing import assert_frame_equal
 
 
 def test_compare():
-    data1 = pd.read_csv("data/companyA.csv")
-    data2 = pd.read_csv("data/companyB.csv")
+    data1 = pd.read_csv("data/input/companyA.csv")
+    data2 = pd.read_csv("data/input/companyB.csv")
     output = compare(data1, data2).reset_index(drop=True)
     expected = pd.read_csv("data/compare.csv")
     assert_frame_equal(expected, output)
