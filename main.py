0ca97f395bd12d3104d4b1f6825021b3268a0396
jameswilliams1 <zcapjwi@ucl.ac.uk>
2019-10-13 14:24:53 +0100

    1 test passed

--- main.py
+++ main.py
@@ -3,4 +3,12 @@ import pandas as pd
 
 
 def compare(df_1, df_2):
-    return pd.merge(pd.merge(df_1, df_2))
+    """Join two dataframes to give a comparison of all prices."""
+    df = pd.merge(df_1, df_2, on="part", how="outer")
+    return df.sort_values("part")
+
+
+if __name__ == "__main__":
+    a = pd.read_csv("data/companyA.csv")
+    b = pd.read_csv("data/companyB.csv")
+    print(compare(a, b))
--- test.py
+++ test.py
@@ -1,5 +1,11 @@
 import pandas as pd
 from main import compare
+from pandas.testing import assert_frame_equal
+
 
 def test_compare():
-    data1 =
+    data1 = pd.read_csv("data/companyA.csv")
+    data2 = pd.read_csv("data/companyB.csv")
+    output = compare(data1, data2).reset_index(drop=True)
+    expected = pd.read_csv("data/compare.csv")
+    assert_frame_equal(expected, output)
