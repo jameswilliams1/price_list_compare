caf67eac6cef9ecf4d6de22f0e5dab3f2aae4f86
jameswilliams1 <zcapjwi@ucl.ac.uk>
2019-10-13 14:59:20 +0100

    refactor tests

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 """Program entry point."""
 import pandas as pd
+import glob
+import os
 
 
 def compare(df_1, df_2):
@@ -8,7 +10,19 @@ def compare(df_1, df_2):
     return df.sort_values("part")
 
 
+def get_frames_from_dir(path="data/input"):
+    """Return a dictionary of name: dataframe from a directory."""
+    all_files = glob.glob(os.path.join(path, "*.csv"))
+    data_dict = {
+        os.path.basename(filename).split(".")[0]: pd.read_csv(
+            filename, index_col="part"
+        )
+        for filename in all_files
+    }
+    return data_dict
+
+
 if __name__ == "__main__":
-    a = pd.read_csv("data/companyA.csv")
-    b = pd.read_csv("data/companyB.csv")
-    print(compare(a, b))
+    a = pd.read_csv("data/input/companyA.csv", index_col="part")
+    b = pd.read_csv("data/input/companyB.csv", index_col="part")
+    # print(compare(a, b))
--- test.py
+++ test.py
@@ -1,11 +1,22 @@
 import pandas as pd
 from main import compare
 from pandas.testing import assert_frame_equal
+import pytest
 
 
-def test_compare():
+@pytest.fixture
+def sample_data():
     data1 = pd.read_csv("data/input/companyA.csv")
     data2 = pd.read_csv("data/input/companyB.csv")
-    output = compare(data1, data2).reset_index(drop=True)
+    return data1, data2
+
+
+def test_compare(sample_data):
+    output = compare(sample_data[0], sample_data[1]).reset_index(drop=True)
     expected = pd.read_csv("data/compare.csv")
     assert_frame_equal(expected, output)
+
+
+def test_get_names_from_dir():
+    folder = "data/input"
+    expected = {"CompanyA": None, "CompanyB": None}
