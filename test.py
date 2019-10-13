848a5fce96b1bc067bc3992fe9a9f70aab12762a
jameswilliams1 <zcapjwi@ucl.ac.uk>
2019-10-13 15:01:04 +0100

    improve test

--- test.py
+++ test.py
@@ -6,17 +6,17 @@ import pytest
 
 @pytest.fixture
 def sample_data():
-    data1 = pd.read_csv("data/input/companyA.csv")
-    data2 = pd.read_csv("data/input/companyB.csv")
+    data1 = pd.read_csv("data/input/companyA.csv", index_col="part")
+    data2 = pd.read_csv("data/input/companyB.csv", index_col="part")
     return data1, data2
 
 
 def test_compare(sample_data):
-    output = compare(sample_data[0], sample_data[1]).reset_index(drop=True)
-    expected = pd.read_csv("data/compare.csv")
+    output = compare(sample_data[0], sample_data[1])
+    expected = pd.read_csv("data/compare.csv", index_col="part")
     assert_frame_equal(expected, output)
 
 
-def test_get_names_from_dir():
+def test_get_names_from_dir(sample_data):
     folder = "data/input"
     expected = {"CompanyA": None, "CompanyB": None}
